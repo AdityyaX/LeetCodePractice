9e9a83c585617ea913cdf23f61b8c4b550da30e6
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2023-11-20 16:09:27 +0530

    Time: 55 ms (21.54%), Space: 41.8 MB (45.69%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -1,16 +1,8 @@
-/**
- * @return {null|boolean|number|string|Array|Object}
- */
 Array.prototype.last = function() {
-   
-    if(this.length==0)
+    // Check if the element is zero then return -1;
+    if(this.length == 0){
         return -1;
-    else{
+    }else{ //Else there are 1 or more elements then simply return element with index array length - 1
         return this[this.length-1];
     }
 };
-
-/**
- * const arr = [1, 2, 3];
- * arr.last(); // 3
- */
