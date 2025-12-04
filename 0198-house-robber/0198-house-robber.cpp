774b247cd1e38eac80e62ea775c2ca91c807a8fb
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-05 01:51:46 +0530

    Time: 0 ms (100%), Space: 10.7 MB (21.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    int robHelper(vector<int>& dp, vector<int>& nums, int i) {
-        if (i >= nums.size())
-            return 0;
+    /*
+    what dp[n] represents : max total amount robbed till nth index including it
+    transition is max = [i-1],cost[n]+[i-2]
+    base case for 0 return 0 index for 1 return max of 0 and 1
+    */
+    /*
+    here got little confused n was passing n instead of n-1 do dry run */
+    int robHelper(vector<int>& nums, vector<int>& dp, int i) {
+        if (i == 0)
+            return nums[0];
+        if (i == 1)
+            return dp[1] = max(nums[0], nums[1]);
 
         if (dp[i] != -1) {
             return dp[i];
         }
-        int left = 0 + robHelper(dp, nums, i + 1);
-        int right = nums[i] + robHelper(dp, nums, i + 2);
-        dp[i] = max(left, right);
-        return dp[i];
-    }
+        int countOne = 0 + robHelper(nums, dp, i - 1);
+        int countTwo = nums[i] + robHelper(nums, dp, i - 2);
 
+        return dp[i] = max(countOne, countTwo);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n + 1, -1);
-        return robHelper(dp, nums, 0);
+        vector<int> dp(n, -1);
+        return robHelper(nums, dp, n - 1);
     }
 };
