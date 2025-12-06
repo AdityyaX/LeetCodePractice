9272f38a1a2d2c021bae38cd28ed1b047bc892ea
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-07 00:27:59 +0530

    Time: 4 ms (37.43%), Space: 9.7 MB (47.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,21 @@
 class Solution {
+/*
+brute force can be sort both of them and match the,
+we can use two hasmaps as well but best approach will be 
+this can be the one of the best approach we can also utilie vector for this
+*/
 public:
     bool isAnagram(string s, string t) {
         if (s.length() != t.length())
             return false;
-
-        vector<int> alph(26, 0);
+        unordered_map<char, int> mpp;
         for (int i = 0; i < s.length(); i++) {
-            alph[s[i] - 'a']++;
-            alph[t[i] - 'a']--;
+            mpp[s[i]]++;
+            mpp[t[i]]--;
         }
-
-        for (int i = 0; i < alph.size(); i++) {
-
-            if (alph[i] != 0) {
+        for (int i = 0; i < s.length(); i++) {
+            if (mpp[s[i]] != 0)
                 return false;
-            }
         }
         return true;
     }
