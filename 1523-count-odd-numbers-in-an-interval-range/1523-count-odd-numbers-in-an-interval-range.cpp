f72a07060a42645aa15bb920a848f04db605f4b3
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-07 16:07:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (38%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,19 +1,9 @@
 class Solution {
     /*
-    brute force approach for this can be let's iterate over array and return value
-    optimized approach can be:-
-    number of odd from 0 to n is n+1/2
-    we can find from low-1 to high with this formula
+    brute force approach for this can be let's iterate over array and return
+    value optimized approach can be:- number of odd from 0 to n is n+1/2 we can
+    find from low-1 to high with this formula
     */
 public:
-    int countOdds(int low, int high) {
-        int count = 0;
-        while (low <= high) {
-            if (low % 2) {
-                count++;
-            }
-            low++;
-        }
-        return count;
-    }
+    int countOdds(int low, int high) { return (high + 1) / 2 - low / 2; }
 };
