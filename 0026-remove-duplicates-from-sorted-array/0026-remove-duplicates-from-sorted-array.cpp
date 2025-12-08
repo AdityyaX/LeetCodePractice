031455cf2a2ea266884f91f6c65a536ad73c4aaf
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-08 23:07:02 +0530

    Time: 0 ms (100%), Space: 22.6 MB (78.02%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,16 @@
 class Solution {
+    /*
+    here we take two pointers and iterate and get tha value */
 public:
     int removeDuplicates(vector<int>& nums) {
-        int index = 0;
-        unordered_set<int> seen;
-        for (int num : nums) {
-            if (seen.find(num) == seen.end()) {
-                seen.insert(num);
-                nums[index] = num;
-                index++;
-            }
+        int n = 0;
+        int m = 0;
+        while (m < nums.size()) {
+            nums[n] = nums[m];
+            while ((m < nums.size() && nums[n] == nums[m]))
+                m++;
+            n++;
         }
-        return index;
+        return n;
     }
 };
