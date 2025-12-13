9bc1a19937c07a30cff9ffad168e57d6c4dbfbf3
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-13 12:07:17 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.59%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     /*
-    this one is brute force approach fid the pattern inside the value*/
+    this one is brute force approach fid the pattern inside the value
+    optimal approach:
+    reverse the whole matrix vertically
+    and then return transpose of the matrix
+    in optimal approach we have same on^2 time but space is o(1)
+    */
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> rotated(n, vector<int>(n));
+        reverse(matrix.begin(), matrix.end());
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                rotated[i][j] = matrix[n - j - 1][i];
+            for (int j = i + 1; j < matrix[i].size(); j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-
-        matrix = rotated;
     }
 };
