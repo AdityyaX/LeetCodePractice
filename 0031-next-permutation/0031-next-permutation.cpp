c7cc72ccf131615e1f668f809ee05722081ad3a0
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-13 22:25:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+
+        int i = nums.size() - 2;
+        while (i >= 0 && nums[i + 1] < nums[i]) {
+            cout << i << endl;
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.size() - 1;
+            while (i > 0 && nums[j] < nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+        reverse(nums.begin() + 1 + i, nums.end());
+    }
+};
