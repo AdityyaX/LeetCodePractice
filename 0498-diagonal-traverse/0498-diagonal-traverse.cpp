19f137efad4e17951f2e8b92b12c8c23513c6058
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-16 22:56:43 +0530

    Time: 95 ms (5.04%), Space: 25.9 MB (27.38%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    /*
-    here i got the approach i should follow but gort confiused with why taking n
-    in innner loop because highest value we can have for i is n amd also the
-    reverse thing is importantt*/
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        int n = mat.size();
+        int m = mat.size(); // rows
+        int n = mat[0].size();
         vector<int> result;
-        int m = mat[0].size();
         int len = n + m - 1;
         for (int d = 0; d < len; d++) {
             vector<int> temp;
-
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < m; i++) {
                 int j = d - i;
-                if (j >= 0 && j < m) {
+                if (j >= 0 && j < n) {
                     temp.push_back(mat[i][j]);
                 }
             }
