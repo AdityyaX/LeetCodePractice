0681c522e722adc5416e5259af6e0940d0fbe627
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-22 12:21:12 +0530

    Time: 0 ms (100%), Space: 10.2 MB (8.54%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.cpp
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -3,26 +3,29 @@ public:
     vector<int> hours = {8, 4, 2, 1};
     vector<int> minutes = {32, 16, 8, 4, 2, 1};
     vector<string> result;
-    void helper(int idx, int turnedOn, int hour, int minute) {
-        if (hour > 11 || minute > 59) return;
+    void helper(int i, int turnedOn, int hour, int minute) {
+        if (hour > 11 || minute > 59)
+            return;
+
         if (turnedOn == 0) {
-            string time = to_string(hour) + ":";
-            if (minute < 10) time += "0";
+            string time = to_string(hour) + ':';
+            if (minute < 10) {
+                time += '0';
+            }
             time += to_string(minute);
             result.push_back(time);
             return;
         }
-
-        if (idx == 10) return;
-
-        if (idx < 4)
-            helper(idx + 1, turnedOn - 1, hour + hours[idx], minute);
-        else
-            helper(idx + 1, turnedOn - 1, hour, minute + minutes[idx - 4]);
-
-        helper(idx + 1, turnedOn, hour, minute);
+        if (i == 10) {
+            return;
+        }
+        if (i < 4) {
+            helper(i + 1, turnedOn - 1, hour + hours[i], minute);
+        } else {
+            helper(i + 1, turnedOn - 1, hour, minute + minutes[i - 4]);
+        }
+        helper(i + 1, turnedOn, hour, minute);
     }
-
     vector<string> readBinaryWatch(int turnedOn) {
         helper(0, turnedOn, 0, 0);
         return result;
