615bd582c2cdb86bc468edbb2b026c2b11bd757d
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-23 15:48:53 +0530

    Time: 111 ms (14.16%), Space: 131.2 MB (13.73%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> result;
-    void helper(int i, int k, vector<int>& combination, int n) {
-        if (combination.size() == k) {
-            result.push_back(combination);
+    void helper(int i, int n, int k, vector<int>& current) {
+
+        if (current.size() == k) {
+            result.push_back(current);
             return;
         }
         if (i == n)
             return;
-        combination.push_back(i + 1);
-        helper(i + 1, k, combination, n);
-        combination.pop_back();
-        helper(i + 1, k, combination, n);
+        current.push_back(i + 1);
+        helper(i + 1, n, k, current);
+        current.pop_back();
+        helper(i + 1, n, k, current);
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int> combination;
-        helper(0, k, combination, n);
+        vector<int> current;
+        helper(0, n, k, current);
         return result;
     }
 };
