098bf17786f05afb8982bc85b5696dedf0b5661b
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-24 22:55:37 +0530

    Time: 0 ms (100%), Space: 9.8 MB (8.29%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
+    unordered_map<char, string> digitToLetters = {
+        {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
+        {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+
     vector<string> result;
-    void helper(int i, unordered_map<char, string> digitToLetters,
-                string digits, string str) {
+    void helper(int i, string current, string digits) {
         if (i == digits.size()) {
-            result.push_back(str);
-            return;
+            result.push_back(current);
         }
-        string letters = digitToLetters[digits[i]];
-        for (char c : letters) {
-            helper(i + 1, digitToLetters, digits, str + c);
+        string temp = digitToLetters[digits[i]];
+        for (char ch : temp) {
+            helper(i + 1, current + ch, digits);
         }
     }
+
     vector<string> letterCombinations(string digits) {
         if (digits.empty())
             return {};
-        unordered_map<char, string> digitToLetters = {
-            {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
-            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
-
-        helper(0, digitToLetters, digits, "");
-
+        string current;
+        helper(0, current, digits);
         return result;
     }
 };
