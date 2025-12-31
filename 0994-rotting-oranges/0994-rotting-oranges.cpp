d463bf5093887b5b725aac24bf0242fe1507eea3
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2025-12-31 23:31:28 +0530

    Time: 0 ms (100%), Space: 17.2 MB (20.09%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,49 +1,58 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid[0].size();
+        int time = 0;
         int n = grid.size();
+        int m = grid[0].size();
         vector<vector<int>> visited(n, vector<int>(m, 0));
-        queue<pair<pair<int, int>, int>> q;
+        queue<pair<int, int>> q;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
+                    q.push({i, j});
                     visited[i][j] = 1;
-                    q.push({{i, j}, 0});
-
                 } else if (grid[i][j] == 0) {
                     visited[i][j] = 1;
                 }
             }
         }
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, -1, 0, 1};
-        int time = 0;
         while (!q.empty()) {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            q.pop();
-            time = max(t, time);
-            for (int i = 0; i < 4; i++) {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    grid[nrow][ncol] == 1) {
-                    visited[nrow][ncol] = 1;
+            int size = q.size(); // number of rotten oranges this minute
+            bool rotted = false;
 
-                    grid[nrow][ncol] = 2;
-                    q.push({{nrow, ncol}, t + 1});
+            for (int k = 0; k < size; k++) {
+                int u = q.front().first;
+                int v = q.front().second;
+                q.pop();
+
+                int drow[] = {-1, 0, 1, 0};
+                int dcol[] = {0, -1, 0, 1};
+
+                for (int i = 0; i < 4; i++) {
+                    int nrow = u + drow[i];
+                    int ncol = v + dcol[i];
+
+                    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                        grid[nrow][ncol] == 1) {
+
+                        grid[nrow][ncol] = 2;
+                        visited[nrow][ncol] = 1;
+                        q.push({nrow, ncol});
+                        rotted = true;
+                    }
                 }
             }
+
+            if (rotted)
+                time++;
         }
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1)
                     return -1;
             }
         }
-
         return time;
     }
 };
