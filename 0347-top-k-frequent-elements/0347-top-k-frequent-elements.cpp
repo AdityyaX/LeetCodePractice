a832408db96105741f1afb7041027729d6e98c7e
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-01-23 19:51:55 +0530

    Time: 4 ms (35.01%), Space: 19.1 MB (24.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    /*
-    here brute force approach which i can think of is creating a hashmap
-    and then store {hashmap.second , hashmap.first } in an array of pair and
-    then create a vector till k return the result
-     */
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> mpp;
-        vector<int> result;
-        vector<pair<int, int>> freqMap;
         for (int i = 0; i < nums.size(); i++) {
             mpp[nums[i]]++;
         }
-        for (auto& freq : mpp) {
-            freqMap.push_back({freq.second, freq.first});
+        vector<pair<int, int>> arr;
+        for (auto mp : mpp) {
+            arr.push_back({mp.second, mp.first});
         }
-        sort(freqMap.rbegin(), freqMap.rend());
-        for (int i = 0; i < k; i++) {
-            result.push_back(freqMap[i].second);
+        sort(arr.rbegin(), arr.rend());
+        vector<int> res;
+        for (int i = 0; i < k; ++i) {
+            res.push_back(arr[i].second);
         }
-        return result;
+        return res;
     }
 };
