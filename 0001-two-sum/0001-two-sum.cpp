8f1b6fd1e8cd0f3e73c3c313e9be7fa7a59376f5
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-01-23 19:29:58 +0530

    Time: 1 ms (76.6%), Space: 14.8 MB (52.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    /*
-    brute force approach can be run two loops and check if sum==target or not if
-    yes return true else false in unordered map search th remaining value
-    one approach can be unordered map which i can think of right now.
-    both one pass and two pass
-    */
     vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
         unordered_map<int, int> mpp;
-        for (int i = 0; i < nums.size(); i++) {
-            int remaining = target - nums[i];
-            if (mpp.count(remaining)) {
-                return {i, mpp[remaining]};
+        for (int i = 0; i < n; i++) {
+            int diff = target - nums[i];
+            if (mpp.find(diff) != mpp.end()) {
+                return {mpp[diff], i};
             }
             mpp[nums[i]] = i;
         }
-        return {};
+        return {-1, -1};
     }
 };
