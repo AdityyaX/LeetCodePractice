6909dafbe839c8238440e34a6860f23063d171aa
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-01-25 22:08:51 +0530

    Time: 0 ms (100%), Space: 8.3 MB (47.17%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    // n = n & (n - 1) removes right most bit from the number;
+    // n = n & (n - 1) removes right most 1 bit from the number;
     int hammingWeight(int n) {
         int count = 0;
         while (n) {
-            n = n & (n - 1);
             count++;
+            n = n & (n - 1);
         }
         return count;
     }
