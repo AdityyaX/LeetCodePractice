df0da091bc21aa9c7f8ae0edcf45fdf635269be4
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-01-26 16:47:15 +0530

    Time: 0 ms (100%), Space: 10.6 MB (62.55%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,27 @@
-/*
-here dp[n] represent maximum amount only but here base condition also includ
-dp[n]==1 transition is also same base condition here can be implement simple
-hoise rebber for 0 to n-2 and 1 to n-1 return max becuase ?*/
 class Solution {
 public:
-    int robLinear(vector<int>& nums, int start, int end, vector<int>& dp) {
+    int helper(vector<int>& nums, vector<int>& dp, int start, int end) {
+        if (end < start)
+            return 0;
+
         if (start == end)
             return nums[start];
-
         if (end - start == 1)
             return max(nums[start], nums[end]);
-
         if (dp[end] != -1)
             return dp[end];
-
-        int skip = robLinear(nums, start, end - 1, dp);
-        int take = nums[end] + robLinear(nums, start, end - 2, dp);
-
+        int take = nums[end] + helper(nums, dp, start, end - 2);
+        int skip = helper(nums, dp, start, end - 1);
         return dp[end] = max(skip, take);
     }
-
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1)
             return nums[0];
-
         vector<int> dp1(n, -1);
         vector<int> dp2(n, -1);
-
-        int case1 = robLinear(nums, 0, n - 2, dp1);
-        int case2 = robLinear(nums, 1, n - 1, dp2);
-
-        return max(case1, case2);
+        int leftCase = helper(nums, dp1, 0, n - 2);
+        int rightCase = helper(nums, dp2, 1, n - 1);
+        return max(leftCase, rightCase);
     }
 };
