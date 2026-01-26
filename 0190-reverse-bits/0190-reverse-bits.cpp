248decbc385091d671bf011b32f259139a6e0e9a
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-01-26 12:38:25 +0530

    Time: 4 ms (29.67%), Space: 8.2 MB (45.75%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    // logic for this is simple just
-    // first lets make space in end of result with 0
-    // replace that zero with last digit of n
-    // move n to right side by right side operator
-    uint32_t reverseBits(uint32_t n) {
+    uint32_t reverseBits(int n) {
         uint32_t result = 0;
         for (int i = 0; i < 32; i++) {
             result <<= 1;
