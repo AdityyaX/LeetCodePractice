b896ced7563fc9f16add169e57af66766fdb772f
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-01-26 12:41:18 +0530

    Time: 0 ms (100%), Space: 21.8 MB (37.28%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int avg=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==i){
-                avg=i+1;
-                continue;
-
-            }
-
-            else{
-                // cout<<i<<endl;
-                return (i);
-            }
+        int n = nums.size();
+        int xor1 = 0;
+        int xor2 = 0;
+        for (int i = 0; i < n; i++) {
+            xor1 = xor1 ^ nums[i];
+            xor2 = xor2 ^ i;
         }
-        return avg;
+        xor2 = xor2 ^ n;
+        return xor2 ^ xor1;
     }
 };
