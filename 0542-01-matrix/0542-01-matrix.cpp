83134bd62e5b42e933989ef189ca426203f99849
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-02-07 02:03:00 +0530

    Time: 19 ms (56.09%), Space: 34.7 MB (84.12%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,33 +3,32 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        queue<pair<int, int>> q;
-        vector<vector<int>> visited(n, vector<int>(m, -1));
         vector<vector<int>> distance(n, vector<int>(m, -1));
+        queue<pair<int, int>> q;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 0) {
-                    visited[i][j] = 1;
                     distance[i][j] = 0;
                     q.push({i, j});
                 }
             }
         }
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, -1, 0, 1};
         while (!q.empty()) {
             auto [x, y] = q.front();
             q.pop();
+            int drow[] = {-1, 0, 1, 0};
+            int dcol[] = {0, -1, 0, 1};
             for (int i = 0; i < 4; i++) {
-                int nx = drow[i] + x;
-                int ny = dcol[i] + y;
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m &&
-                    distance[nx][ny] == -1) {
-                    distance[nx][ny] = distance[x][y] + 1;
-                    q.push({nx, ny});
+                int nrow = x + drow[i];
+                int ncol = y + dcol[i];
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    distance[nrow][ncol] == -1) {
+                    distance[nrow][ncol] = distance[x][y] + 1;
+                    q.push({nrow, ncol});
                 }
             }
         }
+
         return distance;
     }
 };
