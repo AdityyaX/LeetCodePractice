0c72ccef9ae2064f8552c54b0df0a9c2edc5712b
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-02-15 23:29:32 +0530

    Time: 3 ms (13.49%), Space: 12.6 MB (80.79%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& t) {
+        int n = t.size();
+        vector<int> dp = t.back();
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = t[i][j] + min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+};
