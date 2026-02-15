4ec3c20f57dc38be2691a672e1545853428f8625
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-02-15 12:33:57 +0530

    Time: 0 ms (100%), Space: 10.7 MB (44.6%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int robHelper(vector<int>& dp, int n, vector<int>& nums) {
-        if (n < 0)
+    int helper(vector<int>& dp, vector<int>& nums, int i) {
+        if (i < 0)
             return 0;
-        if (n == 0)
+        if (i == 0)
             return nums[0];
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        int lastSecond = nums[n] + robHelper(dp, n - 2, nums);
-        int lastOne = 0 + robHelper(dp, n - 1, nums);
-        return dp[n] = max(lastSecond, lastOne);
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int included = nums[i] + helper(dp, nums, i - 2);
+
+        int excluded = helper(dp, nums, i - 1);
+        dp[i] = max(included, excluded);
+        return dp[i];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n + 1, -1);
-        return robHelper(dp, n - 1, nums);
+        return helper(dp, nums, n - 1);
     }
 };
