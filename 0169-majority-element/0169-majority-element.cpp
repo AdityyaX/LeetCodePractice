90f088d18c4d722cbf4ea43da7480eaa1553e645
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-06-13 21:17:29 +0530

    Time: 0 ms (100%), Space: 28.3 MB (7.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    /*
-    here we can have mulitple sortung hash map let's try this with mooore voting
-    algorithm*/
     int majorityElement(vector<int>& nums) {
+        int i = 0;
+        int n = nums.size();
         int count = 0;
-        int el;
-        for (int i = 0; i < nums.size(); i++) {
+        int el = nums[i];
+        while (i < n) {
             if (count == 0) {
                 el = nums[i];
-                count = 1;
-
-            } else if (nums[i] == el) {
+            }
+            if (nums[i] == el) {
                 count++;
+
             } else {
                 count--;
             }
+            i++;
         }
-        int count1 = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == el)
-                count1++;
+
+        int count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el) {
+                count2++;
+            }
+            if (count2 > n / 2) {
+                return el;
+            }
         }
-        if (count1 >= nums.size() / 2)
-            return el;
         return -1;
     }
 };
