ccd642a7dba24cd5f8ea701176b5310863bbae57
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-06-13 21:05:55 +0530

    Time: 2 ms (3.24%), Space: 8.5 MB (69.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-          double ans = 1.0;
+    // 2^10 4^5 4*4^4 16^2 256^1*2 1024
 
-        long long nn=n;
-        if (nn < 0) nn = -1 * nn;
-        while(nn){
-            if(nn%2==1){
-ans=ans*x;
-nn = nn - 1;
+    double myPow(double x, int n) {
+        long long nn = n;
+        if (nn < 0)
+            nn = -nn;
+        double ans = 1.0;
+        while (nn > 0) {
+            if (nn & 1) {
+                ans = ans * x;
+                cout << ans << endl;
             }
-            else{
-             x=x*x;
-               nn = nn / 2;
-            }
+
+            x *= x;
+            nn >>= 1;
         }
-        if (n < 0) ans = (double)(1.0) / (double)(ans);
-  return ans;
+        return n < 0 ? 1 / ans : ans;
     }
 };
