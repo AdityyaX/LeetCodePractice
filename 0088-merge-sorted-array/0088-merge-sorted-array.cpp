06cbe2e91604f8a43455a605190c01a9ba683e7f
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-06-13 20:02:35 +0530

    Time: 0 ms (100%), Space: 12.1 MB (96.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    /*
-    brute force can be lets add second array in first and the sort the first
-    array*/
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int size = m + n - 1;
         int i = m - 1;
         int j = n - 1;
-        int k = m + n - 1;
-
         while (i >= 0 && j >= 0) {
             if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
+                nums1[size] = nums1[i];
                 i--;
             } else {
-                nums1[k] = nums2[j];
+                nums1[size] = nums2[j];
                 j--;
             }
-            k--;
+            size--;
         }
 
         while (j >= 0) {
-            nums1[k] = nums2[j];
+            nums1[size] = nums2[j];
             j--;
-            k--;
+            size--;
         }
     }
 };
