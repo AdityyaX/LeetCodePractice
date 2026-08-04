d298bae981fd03e47dbe8d377e2e59ee9ff41c4e
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-08-04 23:21:06 +0530

    Time: 19 ms (10.83%), Space: 66.5 MB (22.61%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+    if (intervals.length <= 1) return intervals;
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    const merged = [intervals[0]];
+
+    intervals.sort((a, b) => a-b)
+    for (let i = 1; i < intervals.length; i++) {
+        const current = intervals[i];
+        const lastMerged = merged[merged.length - 1];
+        if (lastMerged[1] >= current[0]) {
+            lastMerged[1] = Math.max(lastMerged[1], current[1]);
+        } else {
+            merged.push(current);
+        }
+    }
+    return merged
+};
