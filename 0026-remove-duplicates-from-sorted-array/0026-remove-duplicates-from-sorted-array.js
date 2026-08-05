801f1208d6df3f34eaf3b1a0b4ff361a00718a79
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-08-06 01:05:45 +0530

    Time: 0 ms (100%), Space: 58 MB (55.75%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function (nums) {
+    if (nums.length === 0) return 0;
+
+    let index = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[index]) {
+            index++;
+            nums[index] = nums[i];
+        }
+    }
+
+    return index + 1;
+};
