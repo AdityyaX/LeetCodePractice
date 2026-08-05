16ac21762afcb113a95d2709fdcadc5a5c2beae5
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-08-06 01:44:41 +0530

    Time: 15 ms (82.45%), Space: 64.3 MB (32.79%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,16 +4,18 @@
  * @return {number}
  */
 var subarraySum = function (nums, k) {
+    const map = new Map();
+    map.set(0, 1);
     let count = 0;
-    for (let i = 0; i < nums.length; i++) {
-        let sum = 0
-        for (let j = i; j < nums.length; j++) {
-            sum += nums[j]
-            if (sum == k) {
-                count++;
-            }
+    let sum = 0;
+    for (let num of nums) {
+        sum += num;
+
+        if (map.has(sum - k)) {
+            count += map.get(sum - k);
         }
+
+        map.set(sum, (map.get(sum) || 0) + 1);
     }
     return count
-
 };
