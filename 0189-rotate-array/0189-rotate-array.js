eaa9a20eeaeb6b1382f3c235fba894e711384801
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-08-06 01:50:06 +0530

    Time: 2 ms (58.92%), Space: 65 MB (63.77%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+    let n = nums.length
+    k = k % n
+
+    reverse(nums, 0, n - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, n - 1);
+
+};
+function reverse(arr, left, right) {
+    while (left < right) {
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+        right--;
+    }
+}
