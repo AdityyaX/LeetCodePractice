53cc70ae6c9d6eb34cde774582b7f250ce135b5b
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2026-08-06 22:26:11 +0530

    Time: 5 ms (82.98%), Space: 55.1 MB (54.09%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,24 +4,15 @@
  * @return {boolean}
  */
 var isIsomorphic = function (s, t) {
-    const mapST = new Map();
-    const mapTS = new Map();
-
+    if (s.length != t.length) return false
+    const a = new Array(256).fill(0)
+    const b = new Array(256).fill(0)
     for (let i = 0; i < s.length; i++) {
-        const ch1 = s[i];
-        const ch2 = t[i];
+        if (a[s[i]] !== b[t[i]]) return false
 
-        if (mapST.has(ch1) && mapST.get(ch1) !== ch2) {
-            return false;
-        }
-
-        if (mapTS.has(ch2) && mapTS.get(ch2) !== ch1) {
-            return false;
-        }
-
-        mapST.set(ch1, ch2);
-        mapTS.set(ch2, ch1);
+        a[s[i]] = i + 1
+        b[t[i]] = i + 1
     }
+    return true
 
-    return true;
 };
